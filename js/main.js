d24429ff587666b83758fd23d54664f50d6f5e34
cKirkktoh <113542793+cKirkktoh@users.noreply.github.com>
2023-10-23 23:30:30 -0400

    added hotspots

---
+++ js/main.js
@@ -0,0 +1,37 @@
+(() => {
+    //console.log("IIFE Fired");
+    //variables
+    const model = document.querySelector("#model");
+    const hotspots = document.querySelectorAll(".Hotspot");
+  
+    //functions
+    function modelLoaded() {
+      //console.log(hotspots);
+      hotspots.forEach(hotspot => {
+        hotspot.style.display = "block";
+      });
+    }
+  
+    function showInfo() {
+      //console.log(this.slot);
+      //console.log(`#${this.slot}`);
+      //since the slot value matches the id value I can use the slot value as a selector to get to the div I want.
+      let selected = document.querySelector(`#${this.slot}`);
+      gsap.to(selected, 1, { autoAlpha: 1 });
+    }
+  
+    function hideInfo() {
+      //console.log(this.slot);
+      //console.log(`#${this.slot}`);
+      let selected = document.querySelector(`#${this.slot}`);
+      gsap.to(selected, 1, { autoAlpha: 0 });
+    }
+  
+    //Event Listener
+    model.addEventListener("load", modelLoaded);
+  
+    hotspots.forEach(function (hotspot) {
+      hotspot.addEventListener("mouseover", showInfo);
+      hotspot.addEventListener("mouseout", hideInfo);
+    });
+  })();
