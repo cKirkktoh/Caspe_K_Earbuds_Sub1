2b43aa1c00f6f15b4e189aa2dac602d641a644c4
cKirkktoh <113542793+cKirkktoh@users.noreply.github.com>
2023-11-12 15:10:25 -0500

    successfully added the scrolling animation
    successfully added the scrolling animation

--- js/main.js
+++ js/main.js
@@ -109,3 +109,49 @@
   
 })();
 
+(() => {
+
+  const canvas = document.querySelector("#explode-view");
+  const context = canvas.getContext("2d");
+  canvas.width = 1920;
+  canvas.height = 1080;
+  const frameCount = 144;
+  const images = [];
+
+  const buds = {
+      frame: 0
+  }
+
+  for(let i=0; i<frameCount; i++) {
+      //console.log(i);
+    const img = new Image();
+  img.src = `images/sequence_${(i+1)}.webp`;  
+  images.push(img);
+  }
+
+  //console.table(images);
+
+  gsap.to(buds, {
+      frame: 143,
+      snap: "frame",
+      scrollTrigger: {
+          trigger: "#explode-view",
+          pin: true,
+          scrub: 1,
+          markers: true,
+          start: "top top"
+      },
+      onUpdate: render
+  })
+
+  images[0].addEventListener("onload", render);
+
+  function render() {
+      console.log(buds.frame);
+      console.log(images[buds.frame]);
+      context.clearRect(0,0, canvas.width, canvas.height);
+      context.drawImage(images[buds.frame],0,0);
+  }
+
+
+})();
